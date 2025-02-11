c15c3b70b41911c3cd476ff37e4b7d45b6e48859
Preethi <-g>
2025-02-11 23:04:56 +0530

    removed

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -1,12 +1,9 @@
-	package com.qa.opencart.pages;
-	
-	public class DemoPage {
-	
-		int m=90;
-		
-	int p=600;
-	
-	
-	
-	
-	}
+package com.qa.opencart.pages;
+
+public class DemoPage {
+
+	int m = 90;
+
+	int p = 600;
+
+}
